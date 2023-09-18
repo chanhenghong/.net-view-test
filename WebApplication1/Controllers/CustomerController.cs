b8a707b55967f7edb15c796b39953f210e46934d
Chanheng Hong <chanheng2002@gmail.com>
2023-09-18 02:49:06 -0500

    test

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:1010/api/customer");
+        private readonly HttpClient _client;
+
+        public CustomerController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult index()
+        {
+            List<CustomerViewModel> customerList = new List<CustomerViewModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+
+                customerList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<CustomerViewModel>>(data);
+                //CustomerViewModel customerList = JsonSerializer.Deserialize<CustomerViewModel>(data);
+               
+
+            }
+
+
+            return View(customerList);
+        }
+
+        public IActionResult addUser()
+        {
+            return View();
+        }
+        public IActionResult createUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult createUser(CustomerViewModel customer)
+        {
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:1010/api/customer");
+                var postJob = client.PostAsJsonAsync<CustomerViewModel>("customer", customer);
+                postJob.Wait();
+
+                var postResult = postJob.Result;
+                if (postResult.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Error");
+
+            return View(customer);
+        }
+    }
+}
---
+++ WebApplication1/Models/CustomerViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.Models
+{
+    public class CustomerViewModel
+    {
+        /*public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }*/
+
+        /*public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public List<String>  results { get; set; }*/
+
+        public int customerNum { get; set; }
+        public string name { get; set; }
+        public string familyName { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public string createDate { get; set; }
+    }
+}
